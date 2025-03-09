aad463284aeb4f70f7d68b55592760d38b9e62f6
divik <diviksaxenaa2610@gmail.com>
2025-03-09 14:08:19 +0530

    trying client side rendering

--- nextjs/my-app/app/page.tsx
+++ nextjs/my-app/app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 export default function Home() {
+
+  const [data, setData] = useState('');
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1').then(response => {
+      setData(response.data)
+      setLoader(false)
+    })
+  }, [])
+
   return(
     <div>
-      lading page
+      {loader ? 'Loading...' : data.body}
     </div>
   )
 }
